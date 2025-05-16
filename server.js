1a356f7e3da5b4c40a3c78ee6ab855ec379be75c
Efemorina <efemorina971@gmail.com>
2025-05-16 14:24:43 +0200

    Remove node_modules from version control

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'; // kendi URL'inle değiştir
+const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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'; // kendi URL'inle değiştir
 
 app.post('/notify', async (req, res) => {
   const { name, product, amount, address } = req.body;
@@ -16,12 +16,14 @@ app.post('/notify', async (req, res) => {
     text: `🛒 Yeni Sipariş\nAd: ${name}\nÜrün: ${product}\nTutar: ${amount}₺\nAdres: ${address}`,
   };
 
-  try {
-    await axios.post(SLACK_WEBHOOK_URL, message);
-    res.status(200).send({ success: true });
-  } catch (err) {
-    res.status(500).send({ success: false, error: err.message });
-  }
+ try {
+  await axios.post(SLACK_WEBHOOK_URL, message);
+  res.status(200).send({ success: true });
+} catch (err) {
+  console.error('Slack gönderme hatası:', err.response ? err.response.data : err.message);
+  res.status(500).send({ success: false, error: err.message });
+}
+
 });
 
 const PORT = process.env.PORT || 3000;
